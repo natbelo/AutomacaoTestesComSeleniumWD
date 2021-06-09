7e58de4221bbe9f9951099057ef989551f48d9c6
natbelo <nataliabelo.nb@gmail.com>
2021-06-09 16:28:50 -0300

    Browserstack

--- src/test/java/suporte/Web.java
+++ src/test/java/suporte/Web.java
@@ -2,10 +2,18 @@ package suporte;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class Web {
+    public static final String AUTOMATE_USERNAME = "natalia_UVau8Y";
+    public static final String AUTOMATE_ACCESS_KEY = "tn8c23SMd3jMQBUToLVh";
+    public static final String URL = "https://" + AUTOMATE_USERNAME + ":" + AUTOMATE_ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
     public static WebDriver createChrome() {
         //Abrindo o navegador
         System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\91\\chromedriver.exe");
@@ -17,5 +25,32 @@ public class Web {
         navegador.get("http://www.juliodelima.com.br/taskit");
 
         return navegador;
+    }
+
+    public static WebDriver createBrowserStack() {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("os_version", "10");
+        caps.setCapability("resolution", "1280x800");
+        caps.setCapability("browser", "Chrome");
+        caps.setCapability("browser_version", "latest");
+        caps.setCapability("os", "Windows");
+        caps.setCapability("name", "BStack-[Java] Sample Test"); // test name
+        caps.setCapability("build", "BStack Build Number 1"); // CI/CD job or build name
+
+        WebDriver navegador = null;
+
+        try {
+            navegador = new RemoteWebDriver(new URL(URL), caps);
+            navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+            //Navegando para a página do Taskit!
+            navegador.get("http://www.juliodelima.com.br/taskit");
+
+        } catch (MalformedURLException e) {
+            System.out.println("Houveram problemas com a URL");
+        }
+
+        return navegador;
+
     }
 }
